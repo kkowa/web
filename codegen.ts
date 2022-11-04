98bb7b0cf4d27250bf0b3e65dff662c1b59490ba
Yuchan Lee <lasuillard@gmail.com>
2022-11-04 12:30:32 +0900

    feat: Update IDL ref to v0.2.0

--- codegen.ts
+++ codegen.ts
@@ -2,7 +2,7 @@ import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "idl/graphql/schemas/server/schema.graphql",
+  schema: "idl/graphql/schemas/server.graphql",
   documents: "src/**/*.tsx",
   generates: {
     "_generated/graphql/server/": {
